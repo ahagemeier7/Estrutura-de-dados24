d82ee06c546b5333217625cd5df91089d22b16c2
Augusto Lima Hagemeier <427281@unoesc.lan>
2024-08-14 22:07:05 -0300

    primeiro projeto .net

---
+++ atividades/aula01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Comentario 1 linha
+/*
+Comentário de multiplas linhas
+*/
+
+//Declarando Variáveis
+
+int number;
+
+//Atribuindo valor
+number = 10;
+
+//Declarando constante
+
+const int DAYS_IN_WEEK = 7;
+
+//imprimindo string concatenada
+Console.WriteLine($"A semana tem {DAYS_IN_WEEK} dias ");
