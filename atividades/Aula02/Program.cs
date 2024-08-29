884501ae1f226b0d9f68b96bc1796222b4781a4a
Augusto Lima Hagemeier <427281@unoesc.lan>
2024-08-28 21:26:47 -0300

    Aula 1,2 e 3

---
+++ atividades/Aula02/Program.cs
@@ -0,0 +1,20 @@
+﻿using Aula02;
+//Tad se refere a tipo abstrato de dados
+
+Person person = new Person();
+Console.WriteLine(person.Name);
+
+Person person1 = new Person("Mauricio Gonzatto", 40);
+Console.WriteLine(person1.Name);
+
+Person person2 = new Person();
+person2.Name = "Vladimir Motoboy";
+person2.Age = 70;
+Console.WriteLine(person2.Name);
+
+
+Person person3 = new Person(){
+    Name = "Leonel Brizola",
+    Age = 80
+};
+Console.WriteLine(person3.Name);
---
+++ atividades/Aula03-1-array/Program.cs
@@ -0,0 +1,33 @@
+﻿// Array unidimensional
+
+// int[] numbers;
+// numbers = new int[5]; //recebe 5 posições
+
+// //atribuição de valor direta ao índice do vetor
+// numbers[0] = 1;
+// numbers[1] = 2;
+// numbers[2] = 3;
+// numbers[3] = 4;
+// numbers[4] = 5;
+
+// //Declaração inicializada
+// string[] names = {"Eu", "Tu", "Ele","Nós", "Vós", "Eles"};
+
+// //Exemplo Vetor com os nomes dos meses
+
+// string[] months = new string[12];
+// for(int i=1; i<=12; i++){
+//     DateTime firstDay = new DateTime(DateTime.Now.Year, i, 1);
+
+//     string name = firstDay.ToString("MMMM");
+
+//     months[i-1] = name;
+// }
+
+// foreach(string m in months){
+//     Console.WriteLine(m);
+// }
+
+
+//--------Atividade
+//Escrever algoritimo que armazene dois vetores separados os primeiros 100 numeros impares e pares
