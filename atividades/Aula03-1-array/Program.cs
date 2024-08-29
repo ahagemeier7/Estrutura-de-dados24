463fc5a056be301864053e73d0cef4569a090ae1
Augusto Lima Hagemeier <427281@unoesc.lan>
2024-08-28 21:57:22 -0300

    exercicio par e impar feito

--- atividades/Aula03-1-array/Program.cs
+++ atividades/Aula03-1-array/Program.cs
@@ -1,4 +1,5 @@
-﻿// Array unidimensional
+﻿//-------------------------------------
+// Array unidimensional
 
 // int[] numbers;
 // numbers = new int[5]; //recebe 5 posições
@@ -15,7 +16,7 @@
 
 // //Exemplo Vetor com os nomes dos meses
 
-// string[] months = new string[12];
+//string[] months = new string[12];
 // for(int i=1; i<=12; i++){
 //     DateTime firstDay = new DateTime(DateTime.Now.Year, i, 1);
 
@@ -28,6 +29,34 @@
 //     Console.WriteLine(m);
 // }
 
-
+//-------------------------------------//-------------------------------------//-------------------------------------//-------------------------------------
 //--------Atividade
-//Escrever algoritimo que armazene dois vetores separados os primeiros 100 numeros impares e pares
+//Escrever algoritimo que armazene dois vetores separados com os primeiros 100 numeros impares e pares
+
+
+int[] pares = new int[50];
+int[] impares = new int[50];
+
+int contpar = 0;
+int contimpar = 0;
+
+for (int i = 1; i <= 100; i++){
+    if(i % 2 == 0){
+        pares[contpar] = i;
+        contpar++;
+    }
+    else{
+        impares[contimpar] = i;
+        contimpar++;
+    }
+}
+
+foreach(int np in pares){
+    Console.WriteLine(np);
+}
+
+Console.WriteLine("----------------------------");
+
+foreach(int ni in impares){
+    Console.WriteLine(ni);
+}
