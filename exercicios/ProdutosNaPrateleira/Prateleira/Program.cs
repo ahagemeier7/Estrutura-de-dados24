65cf055de475620336056fb0e30b6c93a628b120
Augusto Lima Hagemeier <augusto.hagemeier@grupocarboni.local>
2024-10-07 12:57:48 -0300

    exercicio feito

--- exercicios/ProdutosNaPrateleira/Prateleira/Program.cs
+++ exercicios/ProdutosNaPrateleira/Prateleira/Program.cs
@@ -1,39 +1,4 @@
 ﻿using System.Security.Cryptography.X509Certificates;
-using Prateleira;
-//Dictionary<string, Produtos> prateleiras = new Dictionary<string, Produtos>();
-
-// prateleiras.Add("A1",new Produtos(){
-//     Id = 1, 
-//     Nome = "Batata",
-//     Quantidade = 5
-//     }
-// );
-// prateleiras.Add("A2",new Produtos(){
-//     Id = 2, 
-//     Nome = "Arroz",
-//     Quantidade = 5
-//     }
-// );
-// Produtos prodobj;
-// bool x = true;
-// while (x == true){
-//     Console.WriteLine("Digite o Código do produto para procurar");
-//     try{    
-//         string? validador = Console.ReadLine();
-//         if (validador != null){
-//             prateleiras.TryGetValue(validador, out prodobj!);
-//         }else{
-//             Console.WriteLine("Código Invalido! digite outro");    
-//         }
-
-//     }catch(Exception ex){
-//         Console.WriteLine("Código Invalido! digite outro");
-//     }
-    
-
-// }
-
-    
 
 Dictionary<string, string> prateleiras = new(){
     { "1", "A1" },
@@ -50,7 +15,12 @@ Dictionary<string, string> prateleiras = new(){
     { "12", "A3" },
     { "13", "B5" },
     { "14", "B7" },
-    { "15", "A5" }
+    { "15", "A5" },
+    { "16", "B3" },
+    { "17", "C5" },
+    { "118", "C7" },
+    { "19", "A6" },
+    { "20", "A7" }
 };
 
 bool x = true;
@@ -64,11 +34,10 @@ while (x == true){
             if(prateleiras.ContainsKey(validador)){
                 Console.WriteLine($"O produto está na prateleira {prateleiras[validador]}");
             }else{
-                Console.WriteLine("Código Invalido! digite outro"); 
+                Console.WriteLine("Código Invalido! Tente novamente"); 
             }
         }
     }else{
-        Console.WriteLine("Código Invalido! digite outro");  
+        Console.WriteLine("Código Invalido! Tente novamente");  
     }
-
 }
