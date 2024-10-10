d03c397daa6fb62454f0a240de74178b2ca9d8d0
Augusto Lima Hagemeier <427281@unoesc.lan>
2024-10-09 21:55:27 -0300

    ExInsertionSort

---
+++ atividades/algoritimosOrdenacao/BubbleSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using BubbleSort;
+int[] inteiros = [0,12,7,1,2,8];
+
+BubbleSort.BubbleSort.Print(inteiros);
+BubbleSort.BubbleSort.Sort<int>( inteiros );
+BubbleSort.BubbleSort.Print(inteiros);
+
+
+char[] chars = ['x','z','b','c','a'];
+BubbleSort.BubbleSort.Print(chars);
+BubbleSort.BubbleSort.Sort<char>(chars);
+BubbleSort.BubbleSort.Print(chars);
---
+++ exercicios/InsertionSort/Program.cs
@@ -0,0 +1,6 @@
+﻿using InsertionSort;
+int[] numbers = [1,5,3,2,4,6,7,76,23,];
+
+InsertionSort.InsertionSort.Print(numbers);
+InsertionSort.InsertionSort.Sort(numbers);
+InsertionSort.InsertionSort.Print(numbers);
