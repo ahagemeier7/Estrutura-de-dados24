f4eedae18e3679737a2153fef3e12be052917192
Augusto Lima Hagemeier <427281@unoesc.lan>
2024-10-16 21:44:53 -0300

    Exercicio heap

---
+++ exercicios/HeapSort/HeapSort/Program.cs
@@ -0,0 +1,11 @@
+﻿using HeapSort;
+
+
+var array = new int[] { 73, 57, 49, 99, 133, 20, 1 };
+var expected = new int[] { 1, 20, 49, 57, 73, 99, 133 };
+var sortFunction = new HeapSort.HeapSort();
+
+var sortedArray = HeapSort.HeapSort.SortArray(array, array.Length);
+
+Assert.IsNotNull(sortedArray);
+CollectionAssert.AreEqual(sortedArray, expected);
