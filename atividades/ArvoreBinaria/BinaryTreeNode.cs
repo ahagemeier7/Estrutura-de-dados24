1d542d3647318d2d88dcb5caab62f4d92285f7df
Augusto Lima Hagemeier <427281@unoesc.lan>
2024-11-13 20:41:18 -0300

    ex binary tree

--- atividades/ArvoreBinaria/BinaryTreeNode.cs
+++ atividades/ArvoreBinaria/BinaryTreeNode.cs
@@ -7,19 +7,22 @@ namespace ArvoreBinaria
 {
     public class BinaryTreeNode<T>: TreeNode<T>
     {
+
+
         public BinaryTreeNode(){
-            base.Children = new List<TreeNode<T>>();
+            Children = new List<TreeNode<T>>();
         }
 
         public BinaryTreeNode<T> Left{
             get { return (BinaryTreeNode<T>)Children[0]; }
             set { Children[0] = value;}
         }
-
         
         public BinaryTreeNode<T> Right{
             get { return (BinaryTreeNode<T>)Children[1]; }
             set { Children[1] = value;}
         }
+
+
     }
 }
