0d8c916ab48526f34e6789627f22d680165af1be
Augusto Lima Hagemeier <427281@unoesc.lan>
2024-11-13 21:44:12 -0300

    Exercicio quiz pronto

---
+++ exercicios/QuizLivro/QuizItem.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizLivro
+{
+    public class QuizItem
+    {
+        public string Text { get; set; }
+        public QuizItem(string text) => Text = text;
+
+        
+    }
+}
